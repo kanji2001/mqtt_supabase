54798ce6c273105e816fe1458fa53c84084b86e7
nayan Siroya <nayanshiroya55@gmail.com>
2025-07-01 16:26:34 +0530

    fix: change login route method to POST and add CORS support

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userControllers');
 
 router.post('/register', userController.register);
-router.get('/login', userController.login);
+router.post('/login', userController.login);
 // router.post('/generate-certificate', userController.generateCert);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,16 +4,21 @@ const deviceRoutes = require('./routes/deviceRoutes');
 // const certRoutes = require('./routes/certRoutes');
 const errorHandler = require('./middleware/errorHandler');
 const userRoutes = require('./routes/userRoutes');
+const cors = require('cors'); 
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173' 
+}));
+
+
 app.use(express.json());
 
 app.use('/api/devices', deviceRoutes);
-// app.use('/api/certificates', certRoutes);
 app.use('/api/users', userRoutes);
 
-// Common error handler
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(` Server running on http://localhost:${PORT}`));
